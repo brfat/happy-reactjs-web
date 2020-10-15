7b78d6d8af6edc47673fa86540c2610fd76c7c52
Bruno jose da Silva <brunofat@Mac-mini-de-Mac.local>
2020-10-14 23:58:30 -0300

    enviando arquivos js

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h3>FAT no React com Typescript!!!!</h3>
+    </div>
+  );
+}
+
+export default App;
